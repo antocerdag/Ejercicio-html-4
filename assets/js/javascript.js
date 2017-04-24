5e18d6d734f38fe8999186bf89c330e09ff257a8
Antonia Cerda <antoniacerdag@gmail.com>
2017-04-24 18:00:34 -0300

    Creacion de carpetas y html,js y css

---
+++ assets/js/javascript.js
@@ -0,0 +1,21 @@
+var audio = null;
+ function vidplay() {
+       var button = document.getElementById("reproductormusica");
+       if (video.paused) {
+          video.play();
+          button.textContent = "||";
+       } else {
+          video.pause();
+          button.textContent = ">";
+       }
+    }
+
+    function restart() {
+        var video = document.getElementById("Video1");
+        video.currentTime = 0;
+    }
+
+    function skip(value) {
+        var video = document.getElementById("Video1");
+        video.currentTime += value;
+    }      
